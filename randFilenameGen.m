b52445e19fb986e3fb59b627a808ad898a38aab2
Krista Kernodle <Krista@Kristas-MacBook-Pro.local>
2017-12-14 12:47:21 -0500

    create file: randFilenameGen.m

---
+++ randFilenameGen.m
@@ -0,0 +1,18 @@
+%% Random Filename Generator
+% The purpose of this file is to create random and unique filenames that
+% contain lowercase letters and numbers. 
+
+n = 100;
+
+filenames = repmat({''},n,2);
+
+elements = {'a','b','c','d','e','f','g','h','i','j','k','l','m',...
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',...
+            '0','1','2','3','4','5','6','7','8','9'};
+        
+for num = 1:n
+    
+    
+    
+end
+        
