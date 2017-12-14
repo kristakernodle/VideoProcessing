7bab217219b22a35d116655962fab616e5a965f7
kristakernodle <kristakernodle@gmail.com>
2017-12-14 16:23:52 -0500

    includes eartag search within date folders

--- videoRename.m
+++ videoRename.m
@@ -3,6 +3,32 @@
 % all folders, then search those folders for video files that need to be
 % copied to a new location and renamed. 
 
+datesDirectory = dir('/Volumes/HD_Krista/MouseReaching/AutoReaching_TestGroup/ReachingVideos_Date/');
+
+datesFolder = repmat({''},1);
+datesFoldInd = 1;
+
+for dateDirInd = 1:length(datesDirectory)
+    
+   if datesDirectory(dateDirInd).isdir == 1 && length(datesDirectory(dateDirInd).name) > 2
+       datesFolder{datesFoldInd} = datesDirectory(dateDirInd).name;
+       datesFoldInd = datesFoldInd + 1;
+   end
+   
+end
+
+for i = 1:length(datesFolder)
+    animalDirectory = dir(['/Volumes/HD_Krista/MouseReaching/AutoReaching_TestGroup/ReachingVideos_Date/' datesFolder{1}]);
+    
+    for animDirInd = 1:length(animalDirectory)
+        if animalDirectory(animDirInd).isdir == 1 && length(animalDirectory(animDirInd).name) > 2
+            animalFolder{animFoldInd} = animalDirectory(animDirInd).name;
+            animFoldInd = animFoldInd +1;
+        end
+    end
+    
+end
+
 directory = '/Volumes/HD_Krista/MouseReaching/AutoReaching_TestGroup/ReachingVideos_Date/20171207/063_PT1_01/Reaches_02/';
 
 reachVideos = dir([directory '*.mp4']);
