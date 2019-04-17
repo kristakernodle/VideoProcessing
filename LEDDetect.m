e723d447cb4aeb230e0fe73306cac5b17663942c
kristakernodle <kkrista@MSWA0216.umhs.med.umich.edu>
2019-04-17 14:10:59 -0400

    updated to make LED detection dependent on RGB values instead of white pixel values; also updated the frame indexing and started saving start AND stop times

--- LEDDetect.m
+++ LEDDetect.m
@@ -1,14 +1,24 @@
-function LEDDetection(filename)
+function LEDDetect(filename)
+% LEDDetect detects the presence of a blue LED in a video object. 
 
-%% LED Detection Script
-% The purpose of this script is to detect when an LED is turned on in a
-% video frame, save the timestamp associated with the first frame it is on
-% and the last frame it is on, and then cut the original video into the
-% parts only when the LED is on.
+% The purpose of this script is to detect when a blue LED is turned on in a
+% video frame. Time stamp associated with first frame and last frame of LED
+% being on are saved into a .csv file. 
+%
+% This function uses RGB values. See line 31 to change this to a different
+% color space. Note: You will likely have to change the thresh value also.
+%
+% INPUTS: 
+%   filename - full file path for video that will be processed.
+% OUTPUTS:
+%   No variable outputs. 
+%   .csv file is created with same filepath and name as 'filename'
+%
+% 
 
-% Define Directory
-% directory = '/Users/kristakernodle/Documents/GitHub/VideoProcessingMatlab';
-% filename = '760_20180918_T4_01.mp4';
+% Define the pixel threshold for the blue LED being on/off. Note: This
+% value may change depending on frame size, size of LED, etc.
+thresh = 5000;
 
 % Define Output Variables
 reaches = zeros(100,3);
@@ -16,67 +26,36 @@ reaches = zeros(100,3);
 % Import video
 obj = VideoReader(filename);
 
-frameRate = obj.FrameRate; % Get frame rate
-videoDuration = obj.Duration; % Get video duration
-totFrames = frameRate*videoDuration; % Calculate the total number of frames
-
-time = 0;
 reachNum = 0;
 lightOn = 0;
-trialTime = 10;
-intTrialInt = 2;
 
-thresh = 5000;
 tic;
-while time <= videoDuration
-    
-    obj.CurrentTime = time;
+while hasFrame(obj)
     
     vidFrame = readFrame(obj);
-
-    % At the beginning of the video, define where the LED is
-    if time == 0
-
-        xmin = 1;
-        ymin = 560;
-        width = 260;
-        height = 500;
-
-    end
+    time=obj.CurrentTime;
 
     % Detect if LED is on or off
-    bwVidFrame = rgb2gray(vidFrame);
-    binaryFrame = bwVidFrame >= 200;
-
-    whitePix = sum(sum(binaryFrame(ymin:ymin+height,xmin:xmin+width)));
+    bluePix=sum(sum(vidFrame(:,:,3)>250));
     
-    % If the light was previous off but is now on
-    if whitePix >= thresh && lightOn == 0 && time > 7
-            
-            lightOn = 1; % Declare that the light is on
-            reachNum = reachNum + 1; % Define the reach number
-            reaches(reachNum,1) = reachNum; % Save reach number
-            reaches(reachNum,2) = obj.CurrentTime; % Save "time on" of LED
-            time = time + trialTime - (2/frameRate);
-    
+    % If the light is on, was previously off, & time is past the ID card
+    if bluePix >= thresh && lightOn == 0 && time >7
+        lightOn = 1; % Declare that the light is on
+        reachNum = reachNum + 1; % Define the reach number
+        reaches(reachNum,1) = reachNum; % Save reach number
+        reaches(reachNum,2) = time;
+        obj.CurrentTime=time+10;
     % If the light is off
-    elseif whitePix < thresh
-        
-        % If the light was previously on
-        if lightOn == 1
-             reaches(reachNum,3) = obj.CurrentTime; % Save "time off" of LED
-             time = time + (800/frameRate); % Skip forward ~15 seconds
-        end
-        
+    elseif bluePix < thresh && lightOn ==1
+        reaches(reachNum,3) = time; % Save "time off" of LED
         lightOn = 0; % Declare that the light is NOT on
-        time = time + (50/frameRate);
-    else
-        time = time + (25/frameRate);
     end
         
 end
-toc;
 
-csvwrite('760_20180918_T4_01.csv',reaches);
+vidName=strsplit(filename,'.');
+csvwrite([vidName{1} '.csv'],reaches);
+
+toc;
 
 end
