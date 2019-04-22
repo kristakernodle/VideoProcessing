57cca76ea32ea62199f792646186f8e593d65caa
kristakernodle <kristakernodle@gmail.com>
2019-04-22 17:09:00 -0400

    Making modifications for final versions; also making LEDDetect more general

--- auxiliaryFunctions/LEDDetect.m
+++ auxiliaryFunctions/LEDDetect.m
@@ -1,12 +1,11 @@
-function LEDDetect(filename)
-% LEDDetect detects the presence of a blue LED in a video object. 
+function [timeOn timeOff] = LEDDetect(vidName,color_LED,thresh)
+%LEDDetect Determine if an LED is on in video frame.
 
-% The purpose of this script is to detect when a blue LED is turned on in a
-% video frame. Time stamp associated with first frame and last frame of LED
-% being on are saved into a .csv file. 
+%   [TIMEON TIMEOFF] = LEDDETECT(FILENAME, COLOR_LED, THRESH) returns vectors
+%   TIMEON and TIMEOFF for the specified LED color in each frame.
 %
 % This function uses RGB values. See line 31 to change this to a different
-% color space. Note: You will likely have to change the thresh value also.
+% color space.
 %
 % INPUTS: 
 %   filename - full file path for video that will be processed.
@@ -14,22 +13,26 @@ function LEDDetect(filename)
 %   No variable outputs. 
 %   .csv file is created with same filepath and name as 'filename'
 %
-% 
 
-% Define the pixel threshold for the blue LED being on/off. Note: This
-% value may change depending on frame size, size of LED, etc.
-thresh = 5000;
+%
+% By Krista Kernodle, 2019
+% kristakernodle@gmail.com
+% https://github.com/kristakernodle
+%
+
+%%
 
 % Define Output Variables
-reaches = zeros(100,3);
+timeOn = [];
+timeOff = [];
 
 % Import video
-obj = VideoReader(filename);
+obj = VideoReader(vidName);
 
 reachNum = 0;
 lightOn = 0;
 
-tic;
+% Begin processing frames
 while hasFrame(obj)
     
     vidFrame = readFrame(obj);
@@ -38,24 +41,29 @@ while hasFrame(obj)
     % Detect if LED is on or off
     bluePix=sum(sum(vidFrame(:,:,3)>250));
     
-    % If the light is on, was previously off, & time is past the ID card
-    if bluePix >= thresh && lightOn == 0 && time >7
-        lightOn = 1; % Declare that the light is on
+    % If the light is on && was previously off
+    if bluePix >= thresh && lightOn == 0
+        
         reachNum = reachNum + 1; % Define the reach number
         reaches(reachNum,1) = reachNum; % Save reach number
-        reaches(reachNum,2) = time;
-        obj.CurrentTime=time+10;
-    % If the light is off
+        reaches(reachNum,2) = time; % Save "time on" of LED
+        
+        % Make sure that we don't jump beyong video duration (if LED is on
+        % at end of video)
+        if obj.CurrentTime+10 <= obj.Duration
+            obj.CurrentTime=time+10;
+        else
+            obj.CurrentTime=obj.Duration;
+        end
+        
+        lightOn = 1; % Declare that the light is on
+        
+    % If the light is off but was previously on
     elseif bluePix < thresh && lightOn ==1
         reaches(reachNum,3) = time; % Save "time off" of LED
         lightOn = 0; % Declare that the light is NOT on
     end
         
 end
-
-vidName=strsplit(filename,'.');
-csvwrite([vidName{1} '.csv'],reaches);
-
-toc;
 
 end
