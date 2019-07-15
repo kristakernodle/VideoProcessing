aaf011d1520f3257214f96a708900b384c0a7a03
kristakernodle <kristakernodle@gmail.com>
2019-07-15 11:40:18 -0400

    Adding files for running DLC automatically

---
+++ runDLC.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Run files through DLC
+
+Created on Mon Jul 15 11:17:56 2019
+
+@author: kkrista
+"""
+
+# Import necessary packages
+import os
+import deeplabcut
+
+# Import custom functions and variables
+import animalInfo
+import setDLCFunc
+
+# User Define Variables
+DLCDir = '/home/kkrista/Documents/SkilledReaching/'
+cropParamsDir = '/home/kkrista/Documents/SkilledReaching/cropParams/'
+
+# Find the files we want to run
+# For debugging and initial code writing purposes, I've created a specific file to work out of
+
+#### Docker Directory
+# 
+
+#### Linux Directory
+dirToAnalyze = '/media/kkrista/KRISTAEHD/DLCSR/20190712/'
+
